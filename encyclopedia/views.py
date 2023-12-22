584175a4fa7548ca32562ea9fa4fe7c835de79d8
CarlMatthewVerano <113345631+CarlMatthewVerano@users.noreply.github.com>
2023-12-22 23:19:30 +0800

    Feature 1 and 2

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
+from markdown2 import markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def get_page(request, title):
+
+    content = util.get_entry(title.strip())
+
+    if content is None:
+        return render(request, "encyclopedia/errorPage.html")
+
+    content = markdown(content)
+    return render(request, "encyclopedia/titleEntry.html", {
+        "title": title,
+        "content": content
+    })
